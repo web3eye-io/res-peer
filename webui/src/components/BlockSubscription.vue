70ed6b4b3c276c8cb80635f65420c6f6adc48e99
Zhao KK <kikakkz@hotmail.com>
2023-07-18 13:55:11 +0800

    Remove log

--- webui/src/components/BlockSubscription.vue
+++ webui/src/components/BlockSubscription.vue
@@ -15,7 +15,6 @@ const {
 const block = useBlockStore()
 
 onResult((res) => {
-  console.log('block', res)
   const data = res.data as Record<string, Record<string, Record<string, Record<string, unknown>>>>
   block.blockHeight = data.notifications.reason.NewBlock.height as number
   block.blockHash = data.notifications.reason.NewBlock.hash as string
