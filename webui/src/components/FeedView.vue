1832671a43815dab199db00ed2b0e5d371d5eac5
Zhao KK <kikakkz@hotmail.com>
2023-07-18 09:01:53 +0800

    Add margin

--- webui/src/components/FeedView.vue
+++ webui/src/components/FeedView.vue
@@ -2,12 +2,18 @@
   <div
     v-for='[key, _content] in contents'
     :key='key'
+    :style='{width: "100%", margin: "16px 0 16px 0"}'
+    class='row'
   >
-    <div>{{ _content.cid }}</div>
-    <div>{{ _content.title }}</div>
-    <div>{{ _content.content }}</div>
-    <div>{{ _content.likes }}</div>
-    <div>{{ _content.dislikes }}</div>
+    <q-space />
+    <div :style='{width: "540px"}'>
+      <div>{{ _content.cid }}</div>
+      <div>{{ _content.title }}</div>
+      <div v-html='_content.content' />
+      <div>{{ _content.likes }}</div>
+      <div>{{ _content.dislikes }}</div>
+    </div>
+    <q-space />
   </div>
 </template>
 
