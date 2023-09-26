ef5bdd9e2f8afa61a91b3f7cf5cb236ab0fa00c6
Zhao KK <kikakkz@hotmail.com>
2023-09-26 11:48:26 +0800

    We cannot receive block height broadcast currently so we have to poll it

--- webui/src/components/FeedContentsKeysQuery.vue
+++ webui/src/components/FeedContentsKeysQuery.vue
@@ -5,7 +5,7 @@ import gql from 'graphql-tag'
 import { getClientOptions } from 'src/apollo'
 import { useBlockStore } from 'src/stores/block'
 import { useContentStore } from 'src/stores/content'
-import { computed, onMounted, watch } from 'vue'
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
 import { useApplicationStore } from 'src/stores/application'
 import { targetChain } from 'src/stores/chain'
 
@@ -14,6 +14,7 @@ const blockHeight = computed(() => block.blockHeight)
 const content = useContentStore()
 const application = useApplicationStore()
 const feedApp = computed(() => application.feedApp)
+const fetchTicker = ref(-1)
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
 
@@ -27,9 +28,17 @@ const getContentsKeys = () => {
     chainId: targetChain.value
   }))
 
+  // TODO: is it still work
   watch(blockHeight, () => {
     void refetch()
   })
+
+  if (fetchTicker.value >= 0) {
+    window.clearInterval(fetchTicker.value)
+  }
+  fetchTicker.value = window.setInterval(() => {
+    void refetch()
+  }, 3000)
 
   onResult((res) => {
     if (res.loading) {
@@ -48,6 +57,12 @@ watch(feedApp, () => {
 onMounted(() => {
   if (feedApp.value) {
     getContentsKeys()
+  }
+})
+
+onUnmounted(() => {
+  if (fetchTicker.value >= 0) {
+    window.clearInterval(fetchTicker.value)
   }
 })
 
